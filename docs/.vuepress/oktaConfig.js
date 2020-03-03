1e1abe579c19a4801570a8f1ad3b3ec1f2f05bbb
Roberto Borges <roberto.borges@uatt.com.br>
2020-03-03 13:32:19 -0300

    Removed okta-vue

--- docs/.vuepress/oktaConfig.js
+++
@@ -1,6 +0,0 @@
-export const oktaConfig = {
-    issuer: "https://dev-764849.okta.com/oauth2/default",
-    client_id: "0oa2ohl0eHt61OwdE4x6",
-    redirect_uri: "http://localhost:8080/implicit/callback/",
-    scope: "openid profile email"
-};
